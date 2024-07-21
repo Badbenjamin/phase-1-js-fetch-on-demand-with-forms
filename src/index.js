840e1735f699411713024f8c21951f6ef50972c3
ben mccarthy <bmccarthy90@gmail.com>
2024-07-21 16:39:45 -0400

    Fetch Form code along

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 const init = () => {
+
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = document.getElementById("searchByID");
+
+    console.log(input.value);
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        const title = document.getElementById("movieDetails").querySelector("h4")
+        const summary = document.querySelector("section#movieDetails p")
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    })
+
+    })
+    
   
 }
 
