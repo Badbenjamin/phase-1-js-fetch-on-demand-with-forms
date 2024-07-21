c1c49ff0d58dca25610fc3ad0e509668ae757ba3
ben mccarthy <bmccarthy90@gmail.com>
2024-07-21 17:16:59 -0400

    Ben's Fetch On Demand Solution

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 const init = () => {
-
     const inputForm = document.querySelector("form");
     inputForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const input = document.getElementById("searchByID");
+        event.preventDefault();
+        const input = document.getElementById("searchByID");
 
-    console.log(input.value);
+        console.log(input.value);
 
-    fetch(`http://localhost:3000/movies/${input.value}`)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        const title = document.getElementById("movieDetails").querySelector("h4")
-        const summary = document.querySelector("section#movieDetails p")
-        title.innerText = data.title;
-        summary.innerText = data.summary;
-    })
+  
 
+        fetch(`http://localhost:3000/movies/${input.value}`)
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            const title = document.getElementById("movieDetails").querySelector("h4")
+            const summary = document.querySelector("section#movieDetails p")
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+            // if (data = "N") {
+            //     console.log("not found")
+            // }
+            console.log(data);
+            inputForm.reset();
+         })
+    
     })
     
   
